84ed8c71587610e4049fd3efa6ebaec403c11923
Bruce Mc Clure <brucemcc90@gmail.com>
2019-04-12 10:30:55 +1000

    Inserted the routes

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,36 @@
+
+ class ArticlesController < ApplicationController
+
+    def home
+        redirect_to articles_path
+    end
+
+    def index           #show all posts
+
+    end
+
+    def show            #Show single post
+    
+    end
+
+    def create          #Create 1 new article
+
+    end
+
+    def new             #Get for new article
+
+    end
+
+    def edit            #Get for update
+
+    end
+
+    def update          #Update article
+
+    end
+
+    def delete          #Delete article
+
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,25 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  #OPEN PAGE
+  get "/", to: "articles#home", as: "home"                    #home page, to redirect to index
+
+
+  #SHOW ALL ARTICLES & SPECIDIC ARTICLE
+  get "/articles", to: "articles#index", as: "articles"         #all posts
+  get "/articles/:id", to: "articles#show"                      #single post
+
+  #POSTING NEW ARTICLES
+  post "/articles", to: "articles#create", as: "new_article"    #new article
+  get "/articles/new", to: "articles#new"                       #get for new article  
+
+  #UPDATING
+  get "/articles/:id/update", to: "articles#edit"
+  put "/articles/:id/update", to: "articles#update"
+  patch "/articles/:id/update", to: "articles#update" 
+
+  #DELETING
+  #get "/articles/:id", to: "articles#show"
+  delete "/articles/:id", to: "articles#delete"
+
+
 end
